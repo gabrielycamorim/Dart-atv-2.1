f65010827763c73ad99385f911a141e501d3d1ea
gabrielycamorim <gabrielyamorim1@gmail.com>
2020-12-01 18:01:23 -0300

    Add files via upload

---
+++ atv1.dart
@@ -0,0 +1,16 @@
+
+main()
+{
+  var num;
+  var factorial=1;
+  print("O fatorial de");
+  num=7;
+
+  for(int i=1;i<=num;i++)
+  {
+    factorial=factorial*i;
+    
+  }
+    print("${num} é ${factorial}");
+
+}
